a0fbfc2afb732fac2b84d5ed737dcf067d43ff26
Marlon <marlonandrade7928@gmil.com>
2021-02-10 22:59:02 -0500

    se cambio de carpeta

--- index.php
+++ index.php
@@ -12,13 +12,21 @@
         <a class="navbar-brand" href="#">Laravel</a> 
     </nav>    
     
-    <div class="jumbotron jumbotron-fluid p-5 bg-info text-center text-white">
+    <div class="jumbotron  p-5 bg-primary text-black">
         <div class="container ">
             <h1 class="display-4">Consola de PHP</h1>
             <p class="lead">Esto es una consola.</p>
         </div>
     </div>
-
+    <div class="container">
+        <div class="row">
+            <div class="p-3 m-2 bg-info text-white">
+                    <?php
+                        echo "Hola";
+                    ?>
+            </div>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js"   integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
